7074619b864e82afb5a2c3fd21174e4d2469fccf
eduardo191604 <141415558+eduardo191604@users.noreply.github.com>
2024-05-08 09:15:37 -0300

    dxr

--- main.js
+++ main.js
@@ -31,9 +31,9 @@ let minutos = Math.floor(sedundos / 60);
 let horas = Math.floor(minutos / 60);
 let dias = Math.floor(horas / 24);
 
-segundos %=60;
-minutos %=60;
-horas %=24;
+segundos %= 60;
+minutos %= 60;
+horas %= 24;
 
 return dias + "dias" 
 + horas + "horas"
